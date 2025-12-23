1890fa9ea2ea3b48f1491f1d38ff42d4b324201f
Hamza <hamza123ali45678@gmail.com>
2025-12-23 13:02:41 +0500

    feat : add different charts on website

--- src/RootLayer/pages/Bar.tsx
+++ src/RootLayer/pages/Bar.tsx
@@ -1,6 +1,16 @@
-
+import BarCharts from "../../components/shared/BarCharts"
+import { Box } from "@mui/material"
+import Header from "../../components/shared/Header"
 export default function Bar() {
   return (
-    <div>Bar</div>
+    <Box m={"20px"}>
+    <Header title="Bar Chart" subtitle="Simple Bar Charts" />
+
+    <Box height={"75vh"}>
+      <BarCharts />
+
+    </Box>
+
+      </Box>
   )
 }
--- src/RootLayer/pages/Line.tsx
+++ src/RootLayer/pages/Line.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import LineChart from "../../components/shared/LineChart"
+import Header from "../../components/shared/Header"
+import { Box } from "@mui/material"
 
 export default function Line() {
   return (
-    <div>Line</div>
+    <Box>
+          <Header title="Line Chart" subtitle="Simple Line Charts" />
+          <Box height={"75vh"}>
+            <LineChart />
+
+          </Box>
+
+    </Box>
   )
 }
